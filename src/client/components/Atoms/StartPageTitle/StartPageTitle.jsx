401cda80e4242f1491bde6e236eed41fa656dac9
Dag <dagmara.szadkowska@wp.pl>
2024-02-15 12:43:25 +0100

    Created atom for start page title

---
+++ src/client/components/Atoms/StartPageTitle/StartPageTitle.jsx
@@ -0,0 +1,9 @@
+import css from './StartPageTitle.module.css';
+
+export const StartPageTitle = () => {
+  return (
+      <h1 className={css.StartPageTitle}>
+        Welcome to the app!
+      </h1>
+  )
+};
