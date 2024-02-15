4e6ff358a51b2158710e222ff4e01dde85b242ca
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-15 21:34:58 +0100

    Add styles to NavigationLink component

--- src/client/components/Atoms/NavigationLink/NavigationLink.jsx
+++ src/client/components/Atoms/NavigationLink/NavigationLink.jsx
@@ -1,5 +1,10 @@
+import css from './NavigationLink.module.css';
 import { NavLink } from 'react-router-dom';
 
 export const NavigationLink = ({ path, title }) => {
-  return <NavLink to={path}>{title}</NavLink>;
+  return (
+    <NavLink className={css.link} to={path}>
+      {title}
+    </NavLink>
+  );
 };
