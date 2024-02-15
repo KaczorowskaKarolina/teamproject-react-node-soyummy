6c5e257f08b5e75e96bc6db665e9de8336ef9bea
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-15 15:41:39 +0100

    Created verify email function.

--- src/server/controllers/verify.js
+++ src/server/controllers/verify.js
@@ -1,3 +1,20 @@
-async function verify(req, res, next) {}
+import { User } from 'server/models/schema/users/userSchema.js';
+
+async function verify(req, res) {
+  try {
+    const user = await User.findOne({
+      verificationToken: req.params.verificationToken,
+    });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    user.verify = true;
+    user.verificationToken = 'verified';
+    await user.save();
+    return res.status(200).json({ message: 'Verification successful' });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 export { verify };
