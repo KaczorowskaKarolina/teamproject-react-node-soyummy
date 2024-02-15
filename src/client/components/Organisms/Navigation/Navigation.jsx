eb16399847f6053e9a44cf97e4b5ebbd328fed8b
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-15 22:06:32 +0100

    Add logo and close button to Navigation component

--- src/client/components/Organisms/Navigation/Navigation.jsx
+++ src/client/components/Organisms/Navigation/Navigation.jsx
@@ -1,10 +1,14 @@
+import { Logo } from 'client/components/Atoms/Logo/Logo';
 import css from './Navigation.module.css';
 import { Navbar } from 'client/components/Molecules/Navbar/Navbar';
 
 export const Navigation = () => {
   return (
     <div className={css.container}>
-      <div className={css.icons}></div>
+      <div className={css.icons}>
+        <Logo />
+        <button className={css.closeButton}>X</button>
+      </div>
       <Navbar />
     </div>
   );
