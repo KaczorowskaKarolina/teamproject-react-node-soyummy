3e39e008e024b536b4ceb46653ff79509785465e
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-15 15:20:26 +0100

    Creating logout function, that clears user token from db.

--- src/server/controllers/logout.js
+++ src/server/controllers/logout.js
@@ -1,3 +1,18 @@
-async function logout(req, res, next) {}
+import { User } from 'server/models/schema/users/userSchema.js';
+
+export async function logout(req, res) {
+  try {
+    const id = req.user.id;
+    const user = await User.findOne({ id });
+    if (!user || !user.token) {
+      res.status(401).json({ message: 'Unauthorized' });
+    }
+    user.token = null;
+    await user.save();
+    return res.status(204).json('No Content');
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 export { logout };
