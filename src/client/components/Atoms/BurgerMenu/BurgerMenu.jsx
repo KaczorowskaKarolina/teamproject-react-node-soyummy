ceb43f7110ffa91687a369b0c41c888a307a00cb
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-15 23:17:12 +0100

    Create burger menu for navigation

---
+++ src/client/components/Atoms/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,8 @@
+import { ReactComponent as BurgerIcon } from 'client/images/icons/menu-03.svg';
+// import css from './BurgerMenu.module.css';
+
+const BurgerMenu = () => {
+  return <BurgerIcon />;
+};
+
+export { BurgerMenu };
