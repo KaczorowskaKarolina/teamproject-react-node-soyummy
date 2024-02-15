678054364c31cffde4b7ce6da0b9b53630cd3c49
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-16 00:27:57 +0100

    Hide burger menu in desktop view

--- src/client/components/Atoms/BurgerMenu/BurgerMenu.jsx
+++ src/client/components/Atoms/BurgerMenu/BurgerMenu.jsx
@@ -1,8 +1,8 @@
 import { ReactComponent as BurgerIcon } from 'client/images/icons/menu-03.svg';
-// import css from './BurgerMenu.module.css';
+import css from './BurgerMenu.module.css';
 
 const BurgerMenu = () => {
-  return <BurgerIcon />;
+  return <BurgerIcon className={css.burgerIcon} />;
 };
 
 export { BurgerMenu };
