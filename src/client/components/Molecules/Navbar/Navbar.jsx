1c4444936791a3c1fbc9eb9e49efe55eabd3e05a
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-15 13:46:29 +0100

    Add all links to navigation bar

--- src/client/components/Molecules/Navbar/Navbar.jsx
+++ src/client/components/Molecules/Navbar/Navbar.jsx
@@ -3,7 +3,12 @@ import { NavigationLink } from 'client/components/Atoms/NavigationLink/Navigatio
 export const Navbar = () => {
   return (
     <nav>
-      <NavigationLink path="" title="" />
+      <NavigationLink path="/categories/:categoryName" title="Categories" />
+      <NavigationLink path="/add" title="Add recipes" />
+      <NavigationLink path="/my" title="My recipes" />
+      <NavigationLink path="/favorite" title="Favorite" />
+      <NavigationLink path="/shopping-list" title="Shopping list" />
+      <NavigationLink path="/search" title="Search" />
     </nav>
   );
 };
