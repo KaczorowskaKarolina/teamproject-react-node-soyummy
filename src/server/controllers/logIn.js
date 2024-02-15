6a0e3ce3d7c257b0094cc199a70e11583e4926d0
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-15 07:35:44 +0100

    Creating login function. It checks if user with provided email exists and if password is correct and if the user is validate. Also commented code which loads token to db cause not sure if we will do that.

--- src/server/controllers/logIn.js
+++ src/server/controllers/logIn.js
@@ -1,3 +1,32 @@
-async function logIn(req, res, next) {}
+import jwt from 'jsonwebtoken';
+import { User } from 'server/models/schema/users/userSchema.js';
+
+async function logIn(req, res, next) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || user.validatePassword(password)) {
+      return res.status(400).json({ message: 'Email or password incorrect.' });
+    }
+    if (!user.validate) {
+      return res.status(400).json({ message: 'Account not verified' });
+    }
+    const payload = {
+      id: user.id,
+    };
+    const token = jwt.sign(payload, process.env.TOKEN_SECRET, {
+      expiresIn: '1h',
+    });
+    // jesli bedziemy token zapisaywac w db to ponizej jest kod
+    //   user.token = token;
+    //   await user.save();
+    return res.status(200).json({
+      token,
+      user: { email: user.email, name: user.name },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 export { logIn };
