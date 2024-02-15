7585232e1d86157b3a1d3a0ec0cfdadf52c6a093
Dag <dagmara.szadkowska@wp.pl>
2024-02-15 19:12:14 +0100

    Base files for register and login page

---
+++ src/client/components/Atoms/FormInput/FormInput.jsx
@@ -0,0 +1,12 @@
+export const FormInput = () => {
+   return (
+    <div>
+        <form action="">
+            <label htmlFor="">
+                Name
+                <input type="text" />
+            </label>
+        </form>
+    </div>
+   ) 
+}
