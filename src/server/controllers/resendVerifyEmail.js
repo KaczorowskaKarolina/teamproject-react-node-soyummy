e551399038652ffb2d20c62b61a2216c54ce3862
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-15 15:51:46 +0100

    Created resend verify email function.

--- src/server/controllers/resendVerifyEmail.js
+++ src/server/controllers/resendVerifyEmail.js
@@ -1,3 +1,30 @@
-async function resendVerifyEmail(req, res, next) {}
+import { User } from 'server/models/schema/users/userSchema.js';
+import { sendEmail } from 'server/handlers/sendEmail.js';
+
+async function resendVerifyEmail(req, res) {
+  try {
+    const { email } = req.body;
+    if (!email) {
+      return res.status(400).json({ message: 'Missing required field email' });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    if (user.verify) {
+      return res
+        .status(400)
+        .json({ message: 'Verification has already been passed' });
+    }
+    await sendEmail({
+      to: user.email,
+      subject: 'SoYummy - confirm your email!',
+      text: `Hi. To confirm your email please click the button below:`,
+      html: `<button type="button"><a href="http:localhost:3000/users/verify/${user.verificationToken}">Confirm email</a></button>`,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
 
 export { resendVerifyEmail };
