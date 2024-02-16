85d0dcd18e2625d35fb9db800cf579580d17e6fe
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 16:26:30 +0100

    Updating validations in userRoutes because of new variables in handlers/userValidation.js.

--- src/server/routes/users/usersRoutes.js
+++ src/server/routes/users/usersRoutes.js
@@ -4,14 +4,17 @@ import { logIn } from '../../controllers/logIn.js';
 import { signUp } from '../../controllers/signUp.js';
 import { logout } from '../../controllers/logout.js';
 import { currentUser } from '../../controllers/currentUser.js';
-import { userValidation } from '../../handlers/userValidation.js';
+import {
+  userSignUpValidation,
+  userLogInValidation,
+} from '../../handlers/userValidation.js';
 import { authMiddleware } from '../../middlewares/authMiddleware.js';
 import { validationMiddleware } from '../../middlewares/validationMiddleware.js';
 
 const router = Router();
 
-router.post('/signup', validationMiddleware(userValidation), signUp);
-router.post('/login', validationMiddleware(userValidation), logIn);
+router.post('/signup', validationMiddleware(userSignUpValidation), signUp);
+router.post('/login', validationMiddleware(userLogInValidation), logIn);
 router.post('/logout', authMiddleware, logout);
 router.post('/current', authMiddleware, currentUser);
 
