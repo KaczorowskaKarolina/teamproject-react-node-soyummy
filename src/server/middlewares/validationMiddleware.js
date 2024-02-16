e4417b08cb7f0b41fec7c5e13b130301933376b9
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 16:00:47 +0100

    Too many 't' in res.status function. Fixed it.

--- src/server/middlewares/validationMiddleware.js
+++ src/server/middlewares/validationMiddleware.js
@@ -2,7 +2,7 @@ const validationMiddleware = validator => {
   return (req, res, next) => {
     const validation = validator.validate(req.body);
     if (validation.error) {
-      return res.stattus(400).json({ message: validation.error.message });
+      return res.status(400).json({ message: validation.error.message });
     }
     return next();
   };
