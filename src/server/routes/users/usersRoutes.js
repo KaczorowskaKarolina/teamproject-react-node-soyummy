c2aaeb73ea0a1370bfdd04fd24d0754ad64b8188
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 15:32:52 +0100

    Added routes for auth.

--- src/server/routes/users/usersRoutes.js
+++ src/server/routes/users/usersRoutes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
-import { authMiddleware } from 'server/middlewares/authMiddleware.js';
 
 import { logIn } from 'server/controllers/logIn.js';
 import { signUp } from 'server/controllers/signUp.js';
 import { logout } from 'server/controllers/logout.js';
 import { currentUser } from 'server/controllers/currentUser.js';
 import { userValidation } from 'server/handlers/userValidation.js';
+import { authMiddleware } from 'server/middlewares/authMiddleware.js';
 import { validationMiddleware } from 'server/middlewares/validationMiddleware.js';
+
+const router = Router();
+
+router.post('/signup', validationMiddleware(userValidation), signUp);
+router.post('/login', validationMiddleware(userValidation), logIn);
+router.post('/logout', authMiddleware, logout);
+router.post('/current', authMiddleware, currentUser);
+
+export default router;
