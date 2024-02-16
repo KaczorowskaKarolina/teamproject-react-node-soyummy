08caf3560c3741a60492ff1f4b23833f06802c20
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 15:17:24 +0100

    Created function that returns current user.

---
+++ src/server/controllers/currentUser.js
@@ -0,0 +1,15 @@
+import { User } from 'server/models/schema/users/userSchema.js';
+
+async function currentUser(req, res) {
+  try {
+    const user = await User.findById(req.user.id);
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    return res.status(200).json({ data: user });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+}
+
+export { currentUser };
