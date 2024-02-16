b24e4f20125b3954f2b7a0d7e155bcaae439716b
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 17:08:05 +0100

    Adding endpoints to verify account and resend email.

--- src/server/routes/users/usersRoutes.js
+++ src/server/routes/users/usersRoutes.js
@@ -3,12 +3,14 @@ import { Router } from 'express';
 import { logIn } from '../../controllers/logIn.js';
 import { signUp } from '../../controllers/signUp.js';
 import { logout } from '../../controllers/logout.js';
+import { verify } from '../../controllers/verify.js';
 import { currentUser } from '../../controllers/currentUser.js';
 import {
   userSignUpValidation,
   userLogInValidation,
 } from '../../handlers/userValidation.js';
 import { authMiddleware } from '../../middlewares/authMiddleware.js';
+import { resendVerifyEmail } from '../../controllers/resendVerifyEmail.js';
 import { validationMiddleware } from '../../middlewares/validationMiddleware.js';
 
 const router = Router();
@@ -17,5 +19,7 @@ router.post('/signup', validationMiddleware(userSignUpValidation), signUp);
 router.post('/login', validationMiddleware(userLogInValidation), logIn);
 router.post('/logout', authMiddleware, logout);
 router.post('/current', authMiddleware, currentUser);
+router.post('/verify', resendVerifyEmail);
+router.get('/verify/:verificationToken', verify);
 
 export default router;
