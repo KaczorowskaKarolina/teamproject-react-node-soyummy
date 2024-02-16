291ce8fd9a62f5e46415afa8da2ac4488d014f7e
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 15:23:31 +0100

    Created Joi user validation. It is possible to add pattern for password but this we need to discuss.

---
+++ src/server/handlers/userValidation.js
@@ -0,0 +1,13 @@
+import Joi from 'joi';
+
+const userValidation = Joi.object({
+  password: Joi.string().required,
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'pl'] },
+    })
+    .required(),
+}).and('email', 'password');
+
+export { userValidation };
