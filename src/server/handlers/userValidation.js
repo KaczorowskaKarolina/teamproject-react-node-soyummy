6d96e24b7aea44abadb83b78470a24cd6ad12bb7
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-16 16:24:53 +0100

    Making two seperate validation for login and signup because login doesn't need name as a value or even can't have name.

--- src/server/handlers/userValidation.js
+++ src/server/handlers/userValidation.js
@@ -1,6 +1,6 @@
 import Joi from 'joi';
 
-const userValidation = Joi.object({
+const userSignUpValidation = Joi.object({
   password: Joi.string().required(),
   email: Joi.string()
     .email({
@@ -11,4 +11,14 @@ const userValidation = Joi.object({
   name: Joi.string().required(),
 }).and('email', 'password', 'name');
 
-export { userValidation };
+const userLogInValidation = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'pl'] },
+    })
+    .required(),
+}).and('email', 'password');
+
+export { userSignUpValidation, userLogInValidation };
