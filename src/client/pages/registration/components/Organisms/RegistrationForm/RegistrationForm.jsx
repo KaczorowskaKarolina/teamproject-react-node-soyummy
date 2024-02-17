bc55e2a86b3caa44a01d8299875de50ab9a6e820
Dag <dagmara.szadkowska@wp.pl>
2024-02-17 18:20:32 +0100

    Register form added

---
+++ src/client/pages/registration/components/Organisms/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,19 @@
+import { RegistrationInputs } from '../../Molecules/RegistrationInputs/RegistrationInputs.jsx';
+import { RectangleButton } from 'client/components/Atoms/RectangleButton/RectangleButton.jsx';
+import { RegistrationLink } from '../../Atoms/RegistrationLink/RegistrationLink.jsx';
+import css from './RegistrationForm.module.css';
+
+const RegistrationForm = () => {
+  return (
+    <div className={css.RegistrationForm}>
+      <div className={css.RegistrationModal}>
+        <h2 className={css.RegistrationTitle}>Registration</h2>
+        <RegistrationInputs />
+        <RectangleButton title="Sign Up" size="large" />
+      </div>
+      <RegistrationLink />
+    </div>
+  );
+};
+
+export { RegistrationForm };
