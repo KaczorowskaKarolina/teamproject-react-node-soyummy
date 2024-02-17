026170ced995fce51bb2fb744c9b7d099f6f8d93
Dag <dagmara.szadkowska@wp.pl>
2024-02-17 18:19:48 +0100

    Register link atom dome

---
+++ src/client/pages/registration/components/Atoms/RegistrationLink/RegistrationLink.jsx
@@ -0,0 +1,11 @@
+import css from './RegistrationLink.module.css';
+
+const RegistrationLink = () => {
+  return (
+    <a href="#" className={css.RegistrationLink}>
+      Sign in
+    </a>
+  );
+};
+
+export { RegistrationLink };
