784f240f7e12206b352ef442cb1a784ba11d5211
Dag <dagmara.szadkowska@wp.pl>
2024-02-17 16:56:26 +0100

    Sign in form organism done

---
+++ src/client/pages/signIn/components/Organisms/SignInForm/SignInForm.jsx
@@ -0,0 +1,19 @@
+import { SignInInputs } from '../../Molecules/SignInInputs/SignInInputs.jsx';
+import { RectangleButton } from 'client/components/Atoms/RectangleButton/RectangleButton.jsx';
+import { SignInLink } from '../../Atoms/SignInLink/SignInLink.jsx';
+import css from './SignInForm.module.css';
+
+const SignInForm = () => {
+  return (
+    <div className={css.SignInForm}>
+      <div className={css.SignInModal}>
+        <h2 className={css.SignInTitle}>Sign In</h2>
+        <SignInInputs />
+        <RectangleButton title="Sign In" size="large" />
+      </div>
+      <SignInLink />
+    </div>
+  );
+};
+
+export { SignInForm };
