0ec4934b6617a59f3cca54da1669278ea046512c
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-17 22:26:25 +0100

    Improve NavigationLink component styles

--- src/client/components/Atoms/NavigationLink/NavigationLink.jsx
+++ src/client/components/Atoms/NavigationLink/NavigationLink.jsx
@@ -1,10 +1,24 @@
 import css from './NavigationLink.module.css';
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as SearchIcon } from '../../../images/icons/search-icon.svg';
 
-export const NavigationLink = ({ path, title }) => {
+const NavigationLink = ({ path, title, icon }) => {
+  if (icon) {
+    return (
+      <NavLink className={css.link} to={path}>
+        <div className={css.icon}>
+          <SearchIcon />
+        </div>
+        <p className={css.title}>{title}</p>
+      </NavLink>
+    );
+  }
+
   return (
     <NavLink className={css.link} to={path}>
       {title}
     </NavLink>
   );
 };
+
+export { NavigationLink };
