bf2de42b2dfa8838f5e514fc83cdfe0f4112f0b1
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-18 01:05:30 +0100

    Style navigation menu

--- src/client/components/Organisms/Navigation/Navigation.jsx
+++ src/client/components/Organisms/Navigation/Navigation.jsx
@@ -1,15 +1,24 @@
-import { Logo } from 'client/components/Atoms/Logo/Logo';
-import css from './Navigation.module.css';
+import { Link } from 'react-router-dom';
 import { Navbar } from 'client/components/Molecules/Navbar/Navbar';
+import { ReactComponent as CloseIcon } from '../../../images/icons/close-icon.svg';
+import { ReactComponent as LogoIcon } from '../../../images/icons/logo-header-icon.svg';
+import css from './Navigation.module.css';
 
-export const Navigation = () => {
+const Navigation = () => {
   return (
     <div className={css.container}>
       <div className={css.icons}>
-        <Logo />
-        <button className={css.closeButton}>X</button>
+        <Link to="/" className={css.logo}>
+          <LogoIcon />
+        </Link>
+        <button onClick={() => {}} className={css.closeButton}>
+          <CloseIcon />
+        </button>
       </div>
       <Navbar />
+      <div className={css.switch}>switch</div>
     </div>
   );
 };
+
+export { Navigation };
