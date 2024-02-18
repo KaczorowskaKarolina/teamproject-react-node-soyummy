ca52b88d332405b6414364d6923101971e391e64
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-18 11:46:17 +0100

    Fighting with icon svg not displaying.

--- src/client/components/Atoms/AddRecipeImage/AddRecipeImage.jsx
+++ src/client/components/Atoms/AddRecipeImage/AddRecipeImage.jsx
@@ -1,10 +1,11 @@
 import styles from './AddRecipeImage.module.css';
+import Icons from './icons.svg';
 
 const AddRecipeImage = () => {
   return (
     <div className={styles.iconContainer}>
       <svg className={styles.icon} width="64px" height="64px">
-        <use href="../../../images/icons/iconAddRecipeImage.svg#icon-addRecipeImage"></use>
+        <use href={`${Icons}#icon-addRecipeImage`}></use>
       </svg>
     </div>
   );
