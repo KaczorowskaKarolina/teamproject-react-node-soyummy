a4660235e0c44a545c75b7756255b561a5943c8b
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-19 23:26:47 +0100

    Remove links array from component and import it from .json file

--- src/client/components/Molecules/Navbar/Navbar.jsx
+++ src/client/components/Molecules/Navbar/Navbar.jsx
@@ -1,40 +1,8 @@
 import css from './Navbar.module.css';
 import { NavigationLink } from '#atoms/NavigationLink/NavigationLink.jsx';
+import links from './links.json';
 
 const Navbar = () => {
-  const links = [
-    {
-      path: '/categories/:categoryName',
-      title: 'Categories',
-      icon: false,
-    },
-    {
-      path: '/add',
-      title: 'Add recipes',
-      icon: false,
-    },
-    {
-      path: '/my',
-      title: 'My recipes',
-      icon: false,
-    },
-    {
-      path: '/favorite',
-      title: 'Favorite',
-      icon: false,
-    },
-    {
-      path: '/shopping-list',
-      title: 'Shopping list',
-      icon: false,
-    },
-    {
-      path: '/search',
-      title: 'Search',
-      icon: true,
-    },
-  ];
-
   return (
     <nav className={css.navigation}>
       <ul className={css.list}>
