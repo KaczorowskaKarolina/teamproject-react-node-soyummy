653e748234849ce82e45dd73e05f8b60d1a03d26
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-19 14:42:46 +0100

    Style header

--- src/client/components/Organisms/Header/Header.jsx
+++ src/client/components/Organisms/Header/Header.jsx
@@ -5,14 +5,16 @@ import { BurgerMenu } from 'client/components/Atoms/BurgerMenu/BurgerMenu.jsx';
 const Header = () => {
   return (
     <div className={css.container}>
-      <Logo />
-      <div className={css.rightContainer}>
-        <div className={css.userMenu}>
-          <div className={css.avatar}></div>
-          <p className={css.name}>Name</p>
-        </div>
-        <div className={css.burgerIcon}>
+      <div className={css.box}>
+        <Logo />
+        {/* <Navigation /> */}
+        <div className={css.rightContainer}>
+          <div className={css.userMenu}>
+            <div className={css.avatar}></div>
+            <p className={css.name}>Name</p>
+          </div>
           <BurgerMenu />
+          <div className={css.switch}>switch</div>
         </div>
       </div>
     </div>
