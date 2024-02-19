492ef6a399783ceae9bd19bb122c13fcfedc3e90
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-19 13:18:08 +0100

    Improve rendering navigation links

--- src/client/components/Molecules/Navbar/Navbar.jsx
+++ src/client/components/Molecules/Navbar/Navbar.jsx
@@ -2,27 +2,51 @@ import css from './Navbar.module.css';
 import { NavigationLink } from '#atoms/NavigationLink/NavigationLink.jsx';
 
 const Navbar = () => {
+  const links = [
+    {
+      path: '/categories/:categoryName',
+      title: 'Categories',
+      icon: false,
+    },
+    {
+      path: '/add',
+      title: 'Add recipes',
+      icon: false,
+    },
+    {
+      path: '/my',
+      title: 'My recipes',
+      icon: false,
+    },
+    {
+      path: '/favorite',
+      title: 'Favorite',
+      icon: false,
+    },
+    {
+      path: '/shopping-list',
+      title: 'Shopping list',
+      icon: false,
+    },
+    {
+      path: '/search',
+      title: 'Search',
+      icon: true,
+    },
+  ];
+
   return (
     <nav className={css.navigation}>
       <ul className={css.list}>
-        <li className={css.item}>
-          <NavigationLink path="/categories/:categoryName" title="Categories" />
-        </li>
-        <li className={css.item}>
-          <NavigationLink path="/add" title="Add recipes" />
-        </li>
-        <li className={css.item}>
-          <NavigationLink path="/my" title="My recipes" />
-        </li>
-        <li className={css.item}>
-          <NavigationLink path="/favorite" title="Favorite" />
-        </li>
-        <li className={css.item}>
-          <NavigationLink path="/shopping-list" title="Shopping list" />
-        </li>
-        <li className={css.item}>
-          <NavigationLink path="/search" title="Search" icon="true" />
-        </li>
+        {links.map(link => (
+          <li className={css.item}>
+            <NavigationLink
+              path={link.path}
+              title={link.title}
+              icon={link.icon}
+            />
+          </li>
+        ))}
       </ul>
     </nav>
   );
