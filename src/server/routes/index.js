5a693829da724a9bc0bfb6425a1ebbd1fcdaebf5
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-02-19 18:18:06 +0100

    Categories route

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -6,35 +6,12 @@ import { logout } from '#controllers/logout.js';
 import { verify } from '#controllers/verify.js';
 import { authMiddleware } from '#middlewares/authMiddleware.js';
 import { resendVerifyEmail } from '#controllers/resendVerifyEmail.js';
-import { getAllCategoriesList } from '#server/models/schema/categoriesList/categoriesList.js';
-import { getAllRecipes } from '#server/models/schema/recipes/recipes.js';
+import { getAllRecipes, getCategoriesList } from './recipes/recipesRoutes.js';
 
 const router = express.Router();
 
-router.get('/getAllRecipes', async (req, res, next) => {
-  const response = await getAllRecipes();
-  return res.status(200).json({
-    status: 'success',
-    code: 200,
-    data: {
-      file: response,
-    },
-    message: 'nothing to load',
-  });
-});
-
-router.get('/getAllCategoriesList', async (req, res, next) => {
-  const response = await getAllCategoriesList();
-  return res.status(200).json({
-    status: 'success',
-    code: 200,
-    data: {
-      file: response,
-    },
-    message: 'nothing to load',
-  });
-});
-
+router.get('/getAllRecipes', getAllRecipes);
+router.get('/getAllCategoriesList', getCategoriesList);
 router.post('/signUp', signUp);
 router.post('/logIn', logIn);
 router.post('/verify', resendVerifyEmail);
