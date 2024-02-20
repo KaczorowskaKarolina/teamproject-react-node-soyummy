d35702e601a6b83b0e8406eee84e139bca0beca0
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-02-20 14:16:33 +0100

    minor corrections in the code

--- src/client/pages/Page404/components/Atoms/Page404Title/Page404Title.jsx
+++ src/client/pages/Page404/components/Atoms/Page404Title/Page404Title.jsx
@@ -1,7 +1,7 @@
 import css from './Page404Title.module.css';
 
 const Page404Title = () => {
-  return <h1 className={css.Page404Title}>We are sorry,</h1>;
+  return <h1 className={css.Page404Title}><b>We are sorry,</b></h1>;
 };
 
 export { Page404Title };
