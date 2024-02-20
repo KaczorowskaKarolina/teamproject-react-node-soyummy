ac8d2f925b220d3549e353076345eb116b8da37e
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-02-20 14:17:34 +0100

    minor corrections in the cod

--- src/client/pages/Page404/components/Templates/Page404Template.jsx
+++ src/client/pages/Page404/components/Templates/Page404Template.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
+import Page404Image from "#images/pages/Page404/bro.png";
 import { Page404Texts } from '../Molecules/Texts/Page404Texts.jsx';
 import css from './Page404Template.module.css';
 
 const Page404Template = () => {
     return (
         <div className={css.page404_container}>
-            {/* <div>
-                <img src="../../../../images/pages/Page404/bro_mobile.png" alt="people" className={css.page404_image} />
-                </div> */}
-            <div className={css.page404_text}>
+            <img
+                src={Page404Image}
+                alt='People trying to connect'
+                className={css.page404_image} />
+            <div >
                 <Page404Texts />
             </div>
-      </div>
-  );
+        </div>
+    );
 };
 
 export { Page404Template };
