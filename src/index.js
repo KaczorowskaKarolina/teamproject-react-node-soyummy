c9d5421a0a1a6ed0be1db5a44094bb51f0c942ec
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-21 08:08:16 +0100

    Adding some base styles in index.css and importing file in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { App } from 'client/components/App.jsx';
 import './index.css';
+import './client/styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
