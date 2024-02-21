6d6851782643bd472f908122b90f20ece0b284f2
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-21 09:41:51 +0100

    e

--- src/client/components/Organisms/AddIngredients/AddIngredients.jsx
+++ src/client/components/Organisms/AddIngredients/AddIngredients.jsx
@@ -2,13 +2,33 @@ import styles from './AddIngredients.module.css';
 import { AddRecipeHeaders } from '../../Atoms/AddRecipeHeader/AddRecipeHeader.jsx';
 import { AddIngredientsCounter } from 'client/components/Atoms/AddIngredientsCounter/AddIngredientsCounter';
 import { AddIngredientsItem } from '../../Molecules/AddIngredientsItem/AddIngredientsItem';
+import { useEffect, useState } from 'react';
 
 const AddIngredients = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {}, [counter]);
+
+  const handleMinus = () => {
+    setCounter(counter => {
+      counter--;
+    });
+  };
+  const handlePlus = () => {
+    console.log(counter);
+    setCounter(counter => {
+      counter++;
+    });
+  };
   return (
     <div className={styles.AddIngredients}>
       <div className={styles.header}>
         <AddRecipeHeaders isTitle={false}>Ingridients</AddRecipeHeaders>
-        <AddIngredientsCounter />
+        <AddIngredientsCounter
+          minusCb={handleMinus}
+          plusCb={handlePlus}
+          counter={counter}
+        />
       </div>
       <ul className={styles.inputs}>
         <AddIngredientsItem />
