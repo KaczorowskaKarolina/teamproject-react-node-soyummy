afd3234f3813f199abe6ce86dbf1bc29ec6d2aa6
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-21 08:15:55 +0100

    Updating imports.

--- src/client/components/Organisms/AddRecipeForm/AddRecipeForm.jsx
+++ src/client/components/Organisms/AddRecipeForm/AddRecipeForm.jsx
@@ -2,7 +2,7 @@ import styles from './AddRecipeForm.module.css';
 import { AddRecipeImage } from '../../Atoms/AddRecipeImage/AddRecipeImage.jsx';
 import { AddRecipeHeaders } from '../../Atoms/AddRecipeHeader/AddRecipeHeader.jsx';
 import { AddRecipeInfo } from '../AddRecipeInfo/AddRecipeInfo';
-import { AddIngredients } from '../../Molecules/AddIngredientsInput/AddIngredients.jsx';
+import { AddIngredients } from '../../Molecules/AddIngredients/AddIngredients.jsx';
 import { AddRecipeTextarea } from '../../Atoms/AddRecipeTextarea/AddRecipeTextarea.jsx';
 import { CurvedButton } from '../../Atoms/CurvedButton/CurvedButton.jsx';
 
@@ -16,9 +16,9 @@ const AddRecipeForm = () => {
         console.log(form);
       }}
     >
+      <AddRecipeHeaders isTitle={true}>add Recipe</AddRecipeHeaders>
       <AddRecipeImage />
       <AddRecipeInfo />
-      <AddRecipeHeaders isTitle={false}>Ingridients</AddRecipeHeaders>
       <AddIngredients />
       <AddRecipeHeaders isTitle={false}>Recipe Preparetion</AddRecipeHeaders>
       <AddRecipeTextarea />
