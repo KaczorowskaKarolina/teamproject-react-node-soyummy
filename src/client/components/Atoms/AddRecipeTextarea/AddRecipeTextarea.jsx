f53cec8c9c3bd2eccf2f435b7947c17820373125
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-21 08:59:17 +0100

    Changing attribute name to id.

--- src/client/components/Atoms/AddRecipeTextarea/AddRecipeTextarea.jsx
+++ src/client/components/Atoms/AddRecipeTextarea/AddRecipeTextarea.jsx
@@ -3,7 +3,7 @@ import styles from './AddRecipeTextarea.module.css';
 const AddRecipeTextarea = () => {
   return (
     <textarea
-      name="recipeDescription"
+      id="recipeDescription"
       className={styles.AddRecipeDescriptionInput}
       placeholder="Enter recipe"
       type="textarea"
