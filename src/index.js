3f7f1a9687bea6a1b6e2ef92e5d624528d1489bd
Karolina Kaczorowska <122392440+KaczorowskaKarolina@users.noreply.github.com>
2024-02-21 18:29:55 +0100

    add import './client/styles/main.css'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from 'client/components/App.jsx';
 
 import './index.css';
+import './client/styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
