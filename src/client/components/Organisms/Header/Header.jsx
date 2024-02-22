a60c178222294be1a8d8bd6e665f6aea68f28d3c
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-22 20:08:58 +0100

    Create state for opening and closing navigation menu and set rendering dependencies for Navigation component

--- src/client/components/Organisms/Header/Header.jsx
+++ src/client/components/Organisms/Header/Header.jsx
@@ -1,23 +1,33 @@
 import { Logo } from 'client/components/Atoms/Logo/Logo.jsx';
 import css from './Header.module.css';
 import { BurgerMenu } from 'client/components/Atoms/BurgerMenu/BurgerMenu.jsx';
+import { Navbar } from 'client/components/Molecules/Navbar/Navbar';
+import { useState } from 'react';
+import { Navigation } from '../Navigation/Navigation';
 
 const Header = () => {
+  let width = window.innerWidth;
+
+  const [menuState, setMenuState] = useState(false);
+
   return (
-    <div className={css.container}>
-      <div className={css.box}>
-        <Logo />
-        {/* <Navigation /> */}
-        <div className={css.rightContainer}>
-          <div className={css.userMenu}>
-            <div className={css.avatar}></div>
-            <p className={css.name}>Name</p>
+    <>
+      <div className={css.container}>
+        <div className={css.box}>
+          <Logo />
+          {width < 1240 ? null : <Navbar />}
+          <div className={css.rightContainer}>
+            <div className={css.userMenu}>
+              <div className={css.avatar}></div>
+              <p className={css.name}>Name</p>
+            </div>
+            <BurgerMenu openMenu={setMenuState} />
+            <div className={css.switch}>switch</div>
           </div>
-          <BurgerMenu />
-          <div className={css.switch}>switch</div>
         </div>
       </div>
-    </div>
+      {menuState ? <Navigation closeMenu={setMenuState} /> : null}
+    </>
   );
 };
 
