6d4a55d5295c0124400107e9dc14718ac2eb80ba
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-22 16:34:46 +0100

    Changing styles. Added input to upload images.

--- src/client/components/Atoms/AddRecipeImage/AddRecipeImage.jsx
+++ src/client/components/Atoms/AddRecipeImage/AddRecipeImage.jsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import styles from './AddRecipeImage.module.css';
 import { ReactComponent as Icon } from './iconAddRecipeImage.svg';
 
 const AddRecipeImage = () => {
   return (
-    <div className={styles.iconContainer}>
+    <label className={styles.iconContainer} htmlFor="image">
+      <input
+        type="file"
+        name="image"
+        id="recipeImage"
+        accept="image/png, image/jpg, image/jpeg"
+        className={styles.input}
+      />
       <div className={styles.icon}>
         <Icon />
       </div>
-    </div>
+    </label>
   );
 };
 
