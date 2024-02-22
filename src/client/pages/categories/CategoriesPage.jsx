6a89fbc9583cfa63bb36378961100ff69f9953d2
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-02-22 21:20:51 +0100

    Add fetchRecipes function for redux reducer with axios

--- src/client/pages/categories/CategoriesPage.jsx
+++ src/client/pages/categories/CategoriesPage.jsx
@@ -3,19 +3,22 @@ import { useParams } from 'react-router-dom';
 
 import { RecipeTemplate } from './components/Templates/RecipeTemplate.jsx';
 import { CategoryTemplate } from 'client/pages/categories/components/Templates/CategoryTemplate.jsx';
+import { useDispatch } from 'react-redux';
+import { fetchRecipes } from 'client/redux/recipes/operations.js';
 
 const CategoriesPage = () => {
   const { categoryName } = useParams();
   const [recipes, setRecipes] = useState([]);
   const [categories, setCategories] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const categoriesTitleList = await fetchCategories();
-        setCategories(categoriesTitleList);
-        const categoryRecipes = await fetchRecipes();
-        const filteredRecipes = categoryRecipes.filter(
+        // const categoriesTitleList = await fetchCategories();
+        // setCategories(categoriesTitleList);
+        const allRecipes = await dispatch(fetchRecipes());
+        const filteredRecipes = allRecipes.payload.filter(
           recipe => recipe.category === categoryName
         );
         setRecipes(filteredRecipes);
@@ -28,7 +31,7 @@ const CategoriesPage = () => {
 
   return (
     <div>
-      <CategoryTemplate titles={categories} name={categoryName} />
+      {/* <CategoryTemplate titles={categories} name={categoryName} /> */}
       <RecipeTemplate recipes={recipes} />
     </div>
   );
@@ -49,15 +52,4 @@ const fetchCategories = async () => {
   return categoriesArray.map(category => category.title);
 };
 
-const fetchRecipes = async () => {
-  const RecipesResponse = await fetch('http://localhost:3000/getAllRecipes');
-
-  if (!RecipesResponse.ok) {
-    throw new Error('No results');
-  }
-
-  const RecipesResponseData = await RecipesResponse.json();
-  return RecipesResponseData.data.file;
-};
-
-export { CategoriesPage, fetchCategories, fetchRecipes };
+export { CategoriesPage, fetchCategories };
--- src/client/redux/recipes/operations.js
+++ src/client/redux/recipes/operations.js
@@ -1,13 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+axios.defaults.baseURL =
+  process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
 const fetchRecipes = createAsyncThunk(
   'recipes/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/recipes');
-      return response.data.file;
+      const response = await axios.get('/getAllRecipes');
+      const file = response.data.data.file;
+      return file;
     } catch (error) {
+      console.log(error);
       return thunkAPI.rejectWithValue(error.message);
     }
   }
