1f67646ae24da69760da9de8f42153ea60abe933
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-22 07:32:51 +0100

    Wrapping App component into Provider for store and PersistGate for persist.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react.js';
 import { App } from 'client/components/App.jsx';
+import { store, persistor } from 'client/redux/store.js';
 import './client/styles/main.css';
 import 'index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
