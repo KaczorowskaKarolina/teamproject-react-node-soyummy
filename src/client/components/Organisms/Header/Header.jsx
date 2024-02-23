0a3db7663e0a9afe83f0c803a02a4370e7991645
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-23 01:22:41 +0100

    Improve rendering of Navigation component after hamburger menu icon click

--- src/client/components/Organisms/Header/Header.jsx
+++ src/client/components/Organisms/Header/Header.jsx
@@ -2,35 +2,28 @@ import { ReactComponent as HeaderLogo } from '#icons/logo-header-icon.svg';
 import css from './Header.module.css';
 import { BurgerMenu } from '#atoms/BurgerMenu/BurgerMenu.jsx';
 import { Navbar } from '#molecules/Navbar/Navbar';
-import { useState } from 'react';
-import { Navigation } from '../Navigation/Navigation';
 import { Link } from 'react-router-dom';
 
-const Header = () => {
+const Header = ({ openMenu }) => {
   let width = window.innerWidth;
 
-  const [menuState, setMenuState] = useState(false);
-
   return (
-    <>
-      <div className={css.container}>
-        <div className={css.box}>
-          <Link to="/" className={css.logo}>
-            <HeaderLogo />
-          </Link>
-          {width < 1240 ? null : <Navbar />}
-          <div className={css.rightContainer}>
-            <div className={css.userMenu}>
-              <div className={css.avatar}></div>
-              <p className={css.name}>Name</p>
-            </div>
-            <BurgerMenu openMenu={setMenuState} />
-            <div className={css.switch}>switch</div>
+    <div className={css.container}>
+      <div className={css.box}>
+        <Link to="/" className={css.logo}>
+          <HeaderLogo />
+        </Link>
+        {width < 1240 ? null : <Navbar />}
+        <div className={css.rightContainer}>
+          <div className={css.userMenu}>
+            <div className={css.avatar}></div>
+            <p className={css.name}>Name</p>
           </div>
+          <BurgerMenu openMenu={openMenu} />
+          <div className={css.switch}>switch</div>
         </div>
       </div>
-      {menuState ? <Navigation closeMenu={setMenuState} /> : null}
-    </>
+    </div>
   );
 };
 
--- src/client/components/Templates/SharedLayout/SharedLayout.jsx
+++ src/client/components/Templates/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,21 @@
 import { Header } from '#organisms/Header/Header.jsx';
 import { Outlet } from 'react-router-dom';
 import css from './SharedLayout.module.css';
+import { useState } from 'react';
+import { Navigation } from '#organisms/Navigation/Navigation';
 
 const SharedLayout = () => {
+  const [menuState, setMenuState] = useState(false);
+
+  if (menuState) {
+    return <Navigation closeMenu={setMenuState} />;
+  }
   return (
     <div className={css.container}>
-      <Header />
+      <Header openMenu={setMenuState} />
+      <div className={css.titleBox}>
+        <h2 className={css.title}>Title</h2>
+      </div>
       <Outlet />
       {/* <Footer /> */}
     </div>
