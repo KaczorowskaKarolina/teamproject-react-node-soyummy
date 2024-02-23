c3fb6aecc864fa13e34df6eb4aabcdc25063c25a
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-02-23 06:53:32 +0100

    Render elements by media query needs

--- src/client/pages/home/Organisms/HomeOrganism.jsx
+++ src/client/pages/home/Organisms/HomeOrganism.jsx
@@ -1,20 +1,48 @@
+import { useMediaQuery } from '@mui/material';
+
+import css from './HomeOrganism.module.css';
 import { CategoryAtom } from 'client/pages/categories/components/Atoms/categoryAtoms/CategoryAtom';
 import { RecipeOrganism } from 'client/pages/categories/components/Organisms/recipeOrganisms/RecipeOrganism';
 
-const HomeOrganism = ({ categories, recipesByCategory }) => {
-  console.log(recipesByCategory);
-  return (
-    <ul>
-      {categories &&
-        categories.map(category => <CategoryAtom title={category} />)}
-      <ul>
-        {recipesByCategory.map(recipe => {
-          const { thumb, _id, title } = recipe;
-          return <RecipeOrganism imageSource={thumb} title={title} id={_id} />;
-        })}
+const HomeOrganism = ({ recipesByCategory }) => {
+  const countOfElement = CheckMediaQuery();
+  console.log(countOfElement);
+  return recipesByCategory.map(recipe => {
+    return (
+      <ul className={css.HomeOrganism}>
+        <CategoryAtom title={recipe.title} />
+        <ul>
+          {recipe.recipes.map((recipe, index) => {
+            while (index < countOfElement) {
+              return (
+                <RecipeOrganism
+                  imageSource={recipe.thumb}
+                  title={recipe.title}
+                  id={recipe._id}
+                />
+              );
+            }
+          })}
+        </ul>
       </ul>
-    </ul>
-  );
+    );
+  });
 };
+
+function CheckMediaQuery() {
+  const matchesMobile = useMediaQuery('(min-width:20em)');
+  const matchesTablet = useMediaQuery('(min-width:30em)');
+  const matchesDesktop = useMediaQuery('(min-width:48em)');
+
+  if (matchesDesktop) {
+    return 4;
+  }
+  if (matchesTablet) {
+    return 2;
+  }
+  if (matchesMobile) {
+    return 1;
+  }
+}
 
 export { HomeOrganism };
