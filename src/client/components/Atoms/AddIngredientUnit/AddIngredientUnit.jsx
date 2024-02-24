2d3f0b8e3d3f3debf8b14b159b1e074ad5852cf2
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-24 03:22:04 +0100

    Added handlers and new styles.

--- src/client/components/Atoms/AddIngredientUnit/AddIngredientUnit.jsx
+++ src/client/components/Atoms/AddIngredientUnit/AddIngredientUnit.jsx
@@ -1,21 +1,61 @@
 import styles from './AddIngredientUnit.module.css';
+import { AddDropdownList } from '../AddDropdownList/AddDropdownList.jsx';
+import { useState } from 'react';
+import { ReactComponent as Icon } from './iconAddIngredientUnit.svg';
 
 const AddIngredientUnit = () => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const [value, setValue] = useState('');
+  const unitTypes = ['tbs', 'tsp', 'kg', 'g', 'ml', 'l'];
+
+  const handleCloseDropdown = event => {
+    if (!event.target.dataset.scroll) {
+      setOpenDropdown(false);
+      window.removeEventListener('click', handleCloseDropdown);
+      return;
+    }
+    return;
+  };
+
+  const handleOpenDropdown = event => {
+    setOpenDropdown(true);
+    setTimeout(() => {
+      window.addEventListener('click', handleCloseDropdown);
+    }, 100);
+  };
+
+  const changeValue = event => {
+    setValue(event.target.dataset.value);
+  };
+
   return (
     <label htmlFor="ingdredientAmount" className={styles.AddIngredientUnit}>
       <input
         name="amount"
         id="ingdredientAmount"
-        className={styles.input}
+        className={styles.inputAmount}
         type="text"
       />
-      <select id="amountUnit" name="amountUnit" className={styles.select}>
-        <option value="tbs">tbs</option>
-        <option value="ts">ts</option>
-        <option value="kg">kg</option>
-        <option value="g">g</option>
-        <option value="ml">ml</option>
-      </select>
+      <input
+        id="amountUnit"
+        className={styles.inputUnit}
+        readOnly="readOnly"
+        value={value}
+      />
+      <button
+        className={styles.icon}
+        type="button"
+        onClick={handleOpenDropdown}
+      >
+        <Icon />
+      </button>
+      {openDropdown && (
+        <AddDropdownList
+          array={unitTypes}
+          isCentered={true}
+          onItemClick={changeValue}
+        />
+      )}
     </label>
   );
 };
