cdbfed397d81cefaba583c8e2112c5ae4cbdbcf8
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-24 02:58:43 +0100

    Added new component with styles.

---
+++ src/client/components/Atoms/AddDropdownList/AddDropdownList.jsx
@@ -0,0 +1,26 @@
+import styles from './AddDropdownList.module.css';
+
+const AddDropdownList = ({ array, filter, onItemClick }) => {
+  return (
+    <div className={styles.AddDropdownList} data-scroll="">
+      <ul className={styles.list} data-scroll="">
+        {array &&
+          array
+            .filter(item => item.toLowerCase().includes(filter))
+            .map((item, index) => (
+              <li
+                key={index}
+                className={styles.listItem}
+                onClick={onItemClick}
+                data-value={item}
+                data-scroll=""
+              >
+                {item}
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+};
+
+export { AddDropdownList };
