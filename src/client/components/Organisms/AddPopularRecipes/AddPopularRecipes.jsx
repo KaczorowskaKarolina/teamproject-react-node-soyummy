b30512cfd0f910189a60ce6fedc7bfedaaf06c3d
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-24 06:18:26 +0100

    Finishing styling.

--- src/client/components/Organisms/AddPopularRecipes/AddPopularRecipes.jsx
+++ src/client/components/Organisms/AddPopularRecipes/AddPopularRecipes.jsx
@@ -1,10 +1,45 @@
-import { AddRecipeHeaders } from 'client/components/Atoms/AddRecipeHeader/AddRecipeHeader';
 import styles from './AddPopularRecipes.module.css';
+import { AddRecipeHeaders } from '../../Atoms/AddRecipeHeader/AddRecipeHeader.jsx';
+import { AddPopularList } from '../../Molecules/AddPopularList/AddPopularList';
+
+const array = [
+  {
+    title: 'Tsting process',
+    preview: 'none',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur minus modi, eius suscipit voluptatem voluptatibus eaque ab? Ipsum, numquam.',
+  },
+  {
+    title: 'Tsting process',
+    preview: 'none',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur minus modi, eius suscipit voluptatem voluptatibus eaque ab? Ipsum, numquam.',
+  },
+  {
+    title: 'Tsting process',
+    preview: 'none',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur minus modi, eius suscipit voluptatem voluptatibus eaque ab? Ipsum, numquam.',
+  },
+  {
+    title: 'Tsting process',
+    preview: 'none',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur minus modi, eius suscipit voluptatem voluptatibus eaque ab? Ipsum, numquam.',
+  },
+  {
+    title: 'Tsting process',
+    preview: 'none',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores pariatur minus modi, eius suscipit voluptatem voluptatibus eaque ab? Ipsum, numquam.',
+  },
+];
 
 const AddPopularRecipes = () => {
   return (
-    <div>
-      <AddRecipeHeaders isTitle={false}>PopularRecipes</AddRecipeHeaders>
+    <div className={styles.AddPopularRecipes}>
+      <AddRecipeHeaders isTitle={false}>Popular Recipes</AddRecipeHeaders>
+      <AddPopularList recipes={array} />
     </div>
   );
 };
