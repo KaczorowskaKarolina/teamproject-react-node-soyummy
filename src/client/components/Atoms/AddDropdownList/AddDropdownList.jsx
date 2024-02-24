df286876cc3eba34540cc446e1dc677fa9dd2ef4
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-24 07:35:51 +0100

    Making small changes.

--- src/client/components/Atoms/AddDropdownList/AddDropdownList.jsx
+++ src/client/components/Atoms/AddDropdownList/AddDropdownList.jsx
@@ -5,6 +5,7 @@ const AddDropdownList = ({
   filter = '',
   isCentered = false,
   onItemClick,
+  sendDataToParent = () => {},
 }) => {
   const classes = isCentered
     ? `${styles.listItem} ${styles.isCentered}`
@@ -14,16 +15,21 @@ const AddDropdownList = ({
       <ul className={styles.list} data-scroll="">
         {array &&
           array
-            .filter(item => item.toLowerCase().includes(filter))
+            .filter(item =>
+              item.ttl ? item.ttl.toLowerCase().includes(filter) : item
+            )
             .map((item, index) => (
               <li
                 key={index}
                 className={classes}
-                onClick={onItemClick}
-                data-value={item}
+                onClick={event => {
+                  onItemClick(event);
+                  sendDataToParent(item.id ? item.id : item);
+                }}
+                data-value={item.ttl ? item.ttl : item}
                 data-scroll=""
               >
-                {item}
+                {item.ttl ? item.ttl : item}
               </li>
             ))}
       </ul>
