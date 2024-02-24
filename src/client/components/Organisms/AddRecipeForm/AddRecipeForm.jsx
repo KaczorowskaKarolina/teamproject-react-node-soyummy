2a6ad1d31ee9f38c9511fe0f380b73d7b4763ed0
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-24 06:18:42 +0100

    Work in progress.

--- src/client/components/Organisms/AddRecipeForm/AddRecipeForm.jsx
+++ src/client/components/Organisms/AddRecipeForm/AddRecipeForm.jsx
@@ -6,15 +6,27 @@ import { CurvedButton } from '../../Atoms/CurvedButton/CurvedButton.jsx';
 import { AddPreparation } from 'client/components/Molecules/AddPreparation/AddPreparation';
 
 const AddRecipeForm = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const {
+      recipeImage,
+      recipeName,
+      recipeAbout,
+      recipeCategory,
+      recipeTime,
+      recipePreparation,
+    } = form;
+    console.log(
+      recipeName.value,
+      recipeAbout.value,
+      recipeCategory.value,
+      recipeTime.value,
+      recipePreparation.value
+    );
+  };
   return (
-    <form
-      className={styles.AddRecipeForm}
-      onSubmit={event => {
-        event.preventDefault();
-        const form = event.currentTarget;
-        console.log(form);
-      }}
-    >
+    <form className={styles.AddRecipeForm} onSubmit={handleSubmit}>
       <AddRecipeImage />
       <AddRecipeInfo />
       <AddIngredients />
