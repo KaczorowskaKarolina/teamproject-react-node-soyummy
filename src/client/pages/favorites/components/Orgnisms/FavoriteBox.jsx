4b4bd6888cc4b896f85af7465cc259d5e49b721d
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-25 15:53:59 +0100

    Create FavoriteBox component

---
+++ src/client/pages/favorites/components/Orgnisms/FavoriteBox.jsx
@@ -0,0 +1,28 @@
+import { DeleteButton } from '../Atoms/DeleteButton/DeleteButton.jsx';
+import { Image } from '../Atoms/Image/Image.jsx';
+import { RecipeButton } from '../Atoms/RecipeButton/RecipeButton.jsx';
+import { Time } from '../Atoms/Time/Time.jsx';
+import { NameWithDescription } from '../Molecules/NameWithDescription/NameWithDescription.jsx';
+import css from './FavoriteBox.module.css';
+
+const FavoriteBox = () => {
+  return (
+    <div className={css.box}>
+      <div classname={css.image}>
+        <Image />
+      </div>
+      <div className={css.content}>
+        <div className={css.upperContent}>
+          <NameWithDescription />
+          <DeleteButton />
+        </div>
+        <div className={css.lowerContent}>
+          <Time />
+          <RecipeButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { FavoriteBox };
