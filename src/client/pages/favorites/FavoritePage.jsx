e952cd9b631b2c9e1a40d5e25c385f9ac06e608f
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-25 15:56:08 +0100

    Create FavoritePage component

--- src/client/pages/favorites/FavoritePage.jsx
+++ src/client/pages/favorites/FavoritePage.jsx
@@ -1,17 +1,10 @@
-import { DeleteButton } from './components/Atoms/DeleteButton/DeleteButton';
-import { Image } from './components/Atoms/Image/Image';
-import { RecipeButton } from './components/Atoms/RecipeButton/RecipeButton';
-import { Time } from './components/Atoms/Time/Time';
-import { NameWithDescription } from './components/Molecules/NameWithDescription/NameWithDescription';
+import css from './FavoritePage.module.css';
+import { FavoriteBox } from './components/Orgnisms/FavoriteBox.jsx';
 
 const FavoritePage = () => {
   return (
-    <div>
-      <Image />
-      <DeleteButton />
-      <NameWithDescription />
-      <Time />
-      <RecipeButton />
+    <div className={css.container}>
+      <FavoriteBox />
     </div>
   );
 };
