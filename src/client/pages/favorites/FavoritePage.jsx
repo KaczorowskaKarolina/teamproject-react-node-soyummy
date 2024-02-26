28d161100466f6454af31a503c499c630633347b
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-26 01:04:20 +0100

    Set page title in layout

--- src/client/pages/favorites/FavoritePage.jsx
+++ src/client/pages/favorites/FavoritePage.jsx
@@ -1,7 +1,10 @@
 import css from './FavoritePage.module.css';
 import { FavoriteBox } from './components/Orgnisms/FavoriteBox.jsx';
+import { useOutletContext } from 'react-router-dom';
 
 const FavoritePage = () => {
+  const [setTitle] = useOutletContext();
+  setTitle('Favorites');
   return (
     <div className={css.container}>
       <FavoriteBox />
