f59021a51b1859a2ac8062fb3c8be5495527f22d
mattmalicki <mateusz.malicki96@gmail.com>
2024-02-26 15:32:19 +0100

    Forgot to add popular-recipe endpoint in feature/branchExtras.

--- src/server/routes/recipes/recipesRoutes.js
+++ src/server/routes/recipes/recipesRoutes.js
@@ -7,15 +7,18 @@ import { getRecipesByCategory } from '#controllers/recipes/getAllByCategory.js';
 import { getAllIngredients } from '#controllers/ingredients/getAll.js';
 import { getFavorites } from '#controllers/recipes/getFavorites.js';
 import { getRecipesByIngredient } from '#controllers/recipes/getAllByIngredient.js';
+import { getPopularRecipes } from '#controllers/recipes/getPopularRecipes.js';
 
 const router = express.Router();
 
 router.get('/recipes/category-list', getCategories);
 router.get('/recipes', getRecipesQuery);
-router.get('/recipes/:id', getRecipeById);
 router.get('/recipes/category/:category', getRecipesByCategory);
 router.get('/recipes/ingredients/list', getAllIngredients);
 router.get('/recipes/ingredients/:id', getRecipesByIngredient);
 router.get('/recipes/favorites', getFavorites);
+router.get('/recipes/popular-recipe', getPopularRecipes);
+// this has to be at the end!
+router.get('/recipes/:id', getRecipeById);
 
 export default router;
