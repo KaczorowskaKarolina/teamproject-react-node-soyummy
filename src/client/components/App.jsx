30593e9d497e05286620b0cfd8d26158f0c17af4
krystianklimczak <krystianklimczak1996r.com>
2024-02-26 18:35:46 +0100

    Resolve conflicts

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -3,9 +3,7 @@ import { StartPage } from '#pages/startPage/StartPage.jsx';
 import { Registration } from '#pages/registration/Registration.jsx';
 import { SignIn } from '#pages/signIn/SignIn.jsx';
 import { CategoriesPage } from '#pages/categories/CategoriesPage.jsx';
-import { CategoriesPage } from 'client/pages/categories/CategoriesPage.jsx';
 import { HomePage } from 'client/pages/home/HomePage.jsx';
-import { StartPage } from 'client/pages/startPage/StartPage.jsx';
 // import { AddRecipeForm } from './Organisms/AddRecipeForm/AddRecipeForm.jsx';
 
 import { Routes, Route } from 'react-router-dom';
@@ -40,32 +38,14 @@ const App = () => {
 
           <Route path="/recipes/:id" element={<Recipe />} />
 
-          {/* <Route path="*" element={<Page404 />} /> */}
-          {/* <Route index element={<Home />} /> */}
+          <Route path="*" element={<Page404 />} />
+          <Route index element={<HomePage />} />
           {/* <Route path="/search" element={<SearchPage />} /> */}
           {/* <Route path="/add" element={<AddRecipiesPage />} /> */}
           {/* <Route path="/my" element={<MyRecipiesPage />} /> */}
           {/* <Route path="/favorite" element={<FavoritePage />} /> */}
           {/* <Route path="/shopping-list" element={<ShoppingListPage />} /> */}
           <Route path="*" element={<Page404 />} />
-        <Route path="/" element={<SharedLayout />}>
-          <Route
-            path="/categories/:categoryName"
-            element={<CategoriesPage />}
-          />
-          <Route path="/" element={<HomePage />} />
-          {/* <Route index element={<Home />} /> }
-
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/categories/:categoryName" element={<CategoriesPage />} />
-       
-       
-        <Route path="/add" element={<AddRecipiesPage />} />
-        <Route path="/my" element={<MyRecipiesPage />} />
-        <Route path="/favorite" element={<FavoritePage />} />
-        <Route path="/shopping-list" element={<ShoppingListPage />} />
-        <Route path="/search" element={<SearchPage />} />
-        <Route path="*" element={<Page404 />} />  */}
         </Route>
       </Routes>
     </div>
