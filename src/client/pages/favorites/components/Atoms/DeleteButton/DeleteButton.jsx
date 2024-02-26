848106ccddbdf1a1dd74e5fbb099203d6a489c67
Jakub Ławnicki <126253505+JakubLawnicki@users.noreply.github.com>
2024-02-26 13:13:19 +0100

    Improve import path

---
+++ src/client/pages/favorites/components/Atoms/DeleteButton/DeleteButton.jsx
@@ -0,0 +1,14 @@
+import css from './DeleteButton.module.css';
+import { ReactComponent as TrashIcon } from '#icons/trash-icon.svg';
+
+const DeleteButton = () => {
+  return (
+    <button className={css.deleteButton}>
+      <div className={css.iconBox}>
+        <TrashIcon />
+      </div>
+    </button>
+  );
+};
+
+export { DeleteButton };
