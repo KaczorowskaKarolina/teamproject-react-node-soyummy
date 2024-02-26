d0b5a93668021e5fe4f120035584b0f863b8429c
AnnaIstochnikova <anya.istochnikova@gmail.com>
2024-02-26 01:30:30 +0100

    Hide load button if there is no more category to show

--- src/client/pages/home/HomePage.jsx
+++ src/client/pages/home/HomePage.jsx
@@ -11,19 +11,19 @@ import { selectQuery } from 'client/redux/categories/selectors';
 
 const HomePage = () => {
   const [recipesByCategory, setRecipesByCategory] = useState([]);
+  const [hideLoadButton, setHideLoadButton] = useState(false);
   const categoriesOnPage = useSelector(selectQuery);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(changeQuery(0));
-  }, []);
+  }, [dispatch]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const categoriesResponse = await dispatch(fetchCategories());
         const allCategories = await categoriesResponse.payload;
-
         const recipesByCategory = [];
         allCategories.map(category =>
           recipesByCategory.push({
@@ -38,21 +38,36 @@ const HomePage = () => {
         allRecipes.map(recipe => {
           const currentRecipe = recipe;
           const category = recipe.category;
-          recipesByCategory.map(recipe => {
+          return recipesByCategory.map(recipe => {
             if (recipe.title === category) {
               recipe.recipes.push(currentRecipe);
             }
+            return recipe;
           });
         });
 
         setRecipesByCategory(recipesByCategory);
+        if (categoriesOnPage > 4) {
+          window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth',
+          });
+        }
+        if (allCategories.length === 14) {
+          setHideLoadButton(true);
+        }
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
-  }, [categoriesOnPage]);
-  return <HomeTemplate recipesByCategory={recipesByCategory} />;
+  }, [categoriesOnPage, dispatch]);
+  return (
+    <HomeTemplate
+      recipesByCategory={recipesByCategory}
+      hideLoadButton={hideLoadButton}
+    />
+  );
 };
 
 export { HomePage };
--- src/client/pages/home/Templates/HomeTemplate.jsx
+++ src/client/pages/home/Templates/HomeTemplate.jsx
@@ -1,42 +1,27 @@
-import { useDispatch, useSelector } from 'react-redux';
-import css from './HomeTemplate.module.css';
+import { useDispatch } from 'react-redux';
+
+import { HomeOrganism } from '../Organisms/HomeOrganism';
 import { changeQuery } from 'client/redux/categories/operations';
 import { HomeAtomButtonLoadMore } from '../Atoms/HomeAtomButtonLoadMore';
-import { selectCategories } from 'client/redux/categories/selectors';
-const { HomeOrganism } = require('../Organisms/HomeOrganism');
 
-const HomeTemplate = ({ recipesByCategory }) => {
+import css from './HomeTemplate.module.css';
+
+const HomeTemplate = ({ recipesByCategory, hideLoadButton }) => {
   const dispatch = useDispatch();
-  let countOfCategories = useSelector(selectCategories);
   const handleLoadMore = () => {
-    // console.log(countOfCategories);
-    // const differenceOfShownCategories = countOfCategories - query;
     dispatch(changeQuery(4));
   };
   return (
     <div className={css.HomeTemplate}>
       <HomeOrganism recipesByCategory={recipesByCategory} />
-      <HomeAtomButtonLoadMore
-        title="Other categories"
-        onClick={handleLoadMore}
-      />
+      {!hideLoadButton && (
+        <HomeAtomButtonLoadMore
+          title="Other categories"
+          onClick={handleLoadMore}
+        />
+      )}
     </div>
   );
 };
 
 export { HomeTemplate };
-
-// const handleLoadMore = () => {
-//   dispatch(changeQuery(+4));
-//   const differenceOfShownCategories = countOfCategories - query;
-//   // console.log(differenceOfShownCategories);
-//   // console.log(countOfCategories);
-//   if (differenceOfShownCategories < 4) {
-//     dispatch(changeQuery(differenceOfShownCategories));
-//     return;
-//   }
-//   if (countOfCategories.length > query) {
-//     dispatch(changeQuery(+4));
-//     return;
-//   }
-// };
